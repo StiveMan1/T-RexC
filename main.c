0f2840eec27df848028cdc983ecdd2528c9920b4
Sanzhar Zhanalin <04024004zh@gmail.com>
2024-10-31 16:52:46 +0500

    m

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ typedef enum {
     cactus_type_4 = 5,
 } enemy_type;
 
-typedef struct {
+struct game_t {
     int32_t height;
     int32_t weight;
     uint64_t size;
@@ -51,7 +51,7 @@ typedef struct {
     enemy_type e_type;
     int32_t e_x;
     int32_t e_y;
-} game_t;
+} game;
 
 // Input thread to capture key presses
 void *input_thread(void *_) {
@@ -80,17 +80,17 @@ uint64_t get_time() {
     return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
 
-void print_dina(const game_t *game) {
+void print_dina() {
     system("clear");
-    for (int y = game->height - 1; y >= 0; --y) {
-        const uint8_t *screen_raw = game->screen + y * game->weight;
-        for (int x = 0; x < game->weight; ++x) {
+    for (int y = game.height - 1; y >= 0; --y) {
+        const uint8_t *screen_raw = game.screen + y * game.weight;
+        for (int x = 0; x < game.weight; ++x) {
             wprintf(L"%lc", 0x2800 | screen_raw[x]);
         }
         wprintf(L"\n");
     }
 }
-void keyboard_handler(game_t *game) {
+void keyboard_handler() {
     pthread_mutex_lock(&key_mutex);
     const uint8_t c = last_key; // Get the last key pressed
     last_key = '\0';
@@ -110,33 +110,33 @@ void keyboard_handler(game_t *game) {
         break;
         default: ;
     }
-    game->space = space;
-    game->crouch = crouch;
+    game.space = space;
+    game.crouch = crouch;
 }
-void player_movement(game_t *game) {
+void player_movement() {
     const uint64_t score = (get_time() - time_s) / 50;
     uint32_t speed = 3 + score / 300;
     if (speed > 7) speed = 7;
 
     // Jump Calculations
-    if (game->stamp != 0) {
-        int32_t dt = (int32_t) (score - game->stamp) / 2;
+    if (game.stamp != 0) {
+        int32_t dt = (int32_t) (score - game.stamp) / 2;
         dt = (-dt + 8) * dt;
-        game->y = dt > 0 ? dt : 0;
-        if (game->crouch) game->stamp -= 2;
+        game.y = dt > 0 ? dt : 0;
+        if (game.crouch) game.stamp -= 2;
     }
-    if (game->y == 0) game->stamp = game->space ? score - 2 : 0;
-    game->score = score;
-    game->speed = speed;
+    if (game.y == 0) game.stamp = game.space ? score - 2 : 0;
+    game.score = score;
+    game.speed = speed;
 }
-void draw_ground(game_t *game) {
-    const uint64_t size = game->weight / 2 + (game->weight % 2 != 0) + 1;
-    if (size > game->ground_size) {
-        game->ground_size = size;
-        game->ground = malloc(GROUND_H * size * sizeof(uint32_t));
-        memset(game->ground, 0, GROUND_H * size * sizeof(uint32_t));
+void draw_ground() {
+    const uint64_t size = game.weight / 2 + (game.weight % 2 != 0) + 1;
+    if (size > game.ground_size) {
+        game.ground_size = size;
+        game.ground = malloc(GROUND_H * size * sizeof(uint32_t));
+        memset(game.ground, 0, GROUND_H * size * sizeof(uint32_t));
         for (int y = 0; y < GROUND_H; ++y) {
-            uint32_t *_ground_raw = &game->ground[y * size];
+            uint32_t *_ground_raw = &game.ground[y * size];
             for (int x = 0; x < size; ++x) {
                 _ground_raw[x] = ground[y][(rand() & 0x07) == 0x07 ? rand() & 1 : 2];
             }
@@ -144,29 +144,29 @@ void draw_ground(game_t *game) {
     }
 
     for (int y = 0; y < GROUND_H; ++y) {
-        uint32_t *_ground_raw = &game->ground[y * size];
+        uint32_t *_ground_raw = &game.ground[y * size];
         const uint8_t *ground_raw = (uint8_t *)_ground_raw;
-        uint8_t *screen_raw = game->screen + y * game->weight;
+        uint8_t *screen_raw = game.screen + y * game.weight;
 
-        for (int x = 0; x < size * 4 && x < game->weight; ++x) {
-            screen_raw[x] |= ground_raw[(x + game->x) % (size * 4)];
+        for (int x = 0; x < size * 4 && x < game.weight; ++x) {
+            screen_raw[x] |= ground_raw[(x + game.x) % (size * 4)];
         }
-        _ground_raw[(size - 1 + game->x / 4) % size] = ground[y][(rand() & 0x07) == 0x07 ? rand() & 1 : 2];
+        _ground_raw[(size - 1 + game.x / 4) % size] = ground[y][(rand() & 0x07) == 0x07 ? rand() & 1 : 2];
     }
-    game->x = (int32_t) ((game->x + game->speed) % (size * 4));
+    game.x = (int32_t) ((game.x + game.speed) % (size * 4));
 }
-void draw_player(const game_t *game) {
+void draw_player() {
     const uint32_t *tile_dino = NULL;
-    if (game->crouch) tile_dino = game->score & 2 ? down_1 : down_2;
-    else tile_dino = game->score & 2 ? run_1 : run_2;
+    if (game.crouch) tile_dino = game.score & 2 ? down_1 : down_2;
+    else tile_dino = game.score & 2 ? run_1 : run_2;
     const uint32_t *tile_back = NULL;
-    if (game->crouch) tile_back = game->score & 2 ? down_1_r : down_2_r;
-    else tile_back = game->score & 2 ? run_1_r : run_2_r;
+    if (game.crouch) tile_back = game.score & 2 ? down_1_r : down_2_r;
+    else tile_back = game.score & 2 ? run_1_r : run_2_r;
 
     for (int y = 0; y < DINO_H; ++y) {
         const uint32_t *dino_raw = &tile_dino[y * DINO_W];
         const uint32_t *back_raw = &tile_back[y * DINO_W];
-        uint32_t *screen_raw = (uint32_t *) (game->screen + (y + game->y + 1) * game->weight);
+        uint32_t *screen_raw = (uint32_t *) (game.screen + (y + game.y + 1) * game.weight);
         for (int x = 0; x < DINO_W; ++x) {
             screen_raw[x] &= back_raw[x];
             screen_raw[x] |= dino_raw[x];
@@ -188,45 +188,45 @@ uint32_t *get_enemy(const enemy_type type, const uint64_t step) {
         default: return NULL;
     }
 }
-void draw_enemy(game_t *game) {
-    if (game->e_type == 0) {
+void draw_enemy() {
+    if (game.e_type == 0) {
         // TODO random enemy;
-        game->e_type = rand() % ENEMY_TYPES + 1;
-        game->e_x = game->weight;
-        game->e_y = (rand() & 1) && game->e_type == pterodactyl_type ? 5 : 0;
+        game.e_type = rand() % ENEMY_TYPES + 1;
+        game.e_x = game.weight;
+        game.e_y = (rand() & 1) && game.e_type == pterodactyl_type ? 5 : 0;
     }
 
-    uint32_t *enemy_tile = get_enemy(game->e_type, game->score & 2);
+    uint32_t *enemy_tile = get_enemy(game.e_type, game.score & 2);
     if (enemy_tile == NULL) return;
 
     uint8_t ok = 1;
     for (int y = 0; y < ENEMY_H; ++y) {
         const uint8_t *enemy_raw = (uint8_t *) (enemy_tile + y * ENEMY_W);
-        uint8_t *screen_raw = game->screen + (y + game->e_y + 1) * game->weight;
+        uint8_t *screen_raw = game.screen + (y + game.e_y + 1) * game.weight;
         for (int x = 0; x < ENEMY_W * 4; ++x) {
-            if (x + game->e_x < 0) continue;
+            if (x + game.e_x < 0) continue;
             ok = 0;
-            if (x + game->e_x >= w.ws_col) continue;
-            screen_raw[x + game->e_x] |= enemy_raw[x];
+            if (x + game.e_x >= w.ws_col) continue;
+            screen_raw[x + game.e_x] |= enemy_raw[x];
         }
     }
-    game->e_x -= (int32_t) game->speed;
-    if (ok) game->e_type = 0;
+    game.e_x -= (int32_t) game.speed;
+    if (ok) game.e_type = 0;
 }
 
-void update_console_events(game_t *game) {
+void update_console_events() {
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1) return;
     if (w.ws_col == 0) return;
-    game->height = w.ws_row;
-    game->weight = w.ws_col;
+    game.height = w.ws_row;
+    game.weight = w.ws_col;
 
-    if (game->weight * game->height > game->size) {
-        if (game->screen != NULL) free(game->screen);
-        game->size = game->weight * game->height;
-        game->screen = malloc(game->size);
+    if (game.weight * game.height > game.size) {
+        if (game.screen != NULL) free(game.screen);
+        game.size = game.weight * game.height;
+        game.screen = malloc(game.size);
 
     }
-    memset(game->screen, 0, game->size);
+    memset(game.screen, 0, game.size);
 
     keyboard_handler(game);
     player_movement(game);
@@ -237,7 +237,6 @@ void update_console_events(game_t *game) {
 
 // Drawing thread to simulate console drawing
 void *drawing_thread(void *arg) {
-    game_t game = {};
     while (running) {
         usleep(32000);
         update_console_events(&game);
